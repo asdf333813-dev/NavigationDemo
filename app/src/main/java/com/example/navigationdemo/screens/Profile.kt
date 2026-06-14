1c21dc4c3f9a51dd7b534add37ca915ef7d4811b
Андрей <asdf333813@gmail.com>
2026-06-14 14:19:25 +0700

    Add Home Welcome and Profile screens

---
+++ app/src/main/java/com/example/navigationdemo/screens/Profile.kt
@@ -0,0 +1,39 @@
+package com.example.navigationdemo.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Profile(
+    onClearBackStack: () -> Unit
+) {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Text(
+                "Setup your profile",
+                style = MaterialTheme.typography.headlineSmall
+            )
+
+            Spacer(modifier = Modifier.size(30.dp))
+
+            Button(
+                onClick = {
+                }
+            ) {
+                Text(text = "Go Home")
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/navigationdemo/screens/Welcome.kt
@@ -0,0 +1,41 @@
+package com.example.navigationdemo.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation3.runtime.NavKey
+
+@Composable
+fun Welcome(
+    onNavigation: (NavKey) -> Unit,
+    name: String = ""
+) {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Text(
+                "Welcome $name",
+                style = MaterialTheme.typography.headlineSmall
+            )
+
+            Spacer(modifier = Modifier.size(30.dp))
+
+            Button(
+                onClick = {
+                }
+            ) {
+                Text(text = "Set up your Profile")
+            }
+        }
+    }
+}
